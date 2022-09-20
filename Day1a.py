c1fcfc51f5831325094286491f56f11671e83376
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-09-20 23:13:10 +0530

    Add files via upload

---
+++ Day1a.py
@@ -0,0 +1,91 @@
+# print("Hello World")
+
+
+# age =20
+# age =25
+# print(age)
+# print("age is",age)
+
+
+# price=20.25
+# print(price)
+# print("price of rice is",price)
+
+
+# first_name='Chandra Shekar Reddy'
+# is_online=True
+
+
+# name=input("What is your name? ")
+# print("hello "+name)
+
+
+# birth_year=input("Enter your birth year: ")
+# age=2022-int(birth_year)
+# print(age)
+# n=str(age)
+# print("your age is "+n)
+# print("you are "+n+" years old")
+  
+
+# First_Number=input("First Number: ")
+# Second_Number=input("Second Number: ")       
+# sum=int(First_Number)+int(Second_Number)
+"""n=int(First_Number)
+m=int(Second_Number)    
+sum=n+m
+print(sum)"""
+# print("sum is: "+str(sum))
+
+
+# course="Python for Biginners"
+# print(course)                   #Python for Biginners
+# print(course.upper())           #PYTHON FOR BIGINNERS
+# print(course.lower())           #python for biginners
+# print(course.isupper())         #False
+# print(course.isdigit())         #False
+# print(course.islower())         #False
+# print(course.find('B'))         #11
+# print(course.find('N'))         #-1
+# print(course.find('for'))       #7 
+# print(course.find('n'))         #5
+# print(course.replace('for','4'))    #Python 4 Biginners
+# print('Python' in course)           #True
+
+# x=10
+# x=x+3
+""" x=x+3    x+=3
+x=x-3    x-=3
+x=x*3    x*=3 """
+# print(x)
+
+
+# y=10+3*4
+# print(y)
+# z=(10+3)*6
+# print(z)
+
+
+# a=3>2
+# print(a)            #True
+# b=3==2
+# print(b)            #False
+
+
+# price=40
+# print(price>30 and price<50)  #True
+# print(price>35 or price<50)  #True
+# print(not price>5)     #False
+
+
+# temp=35
+# if temp>30:
+#     print("It's a hot day")
+#     print("Drink lot of Water")
+# elif temp>20:                       #(20,30]
+#     print("It's a nice day")
+# elif temp>10:
+#     print("It's a bit cold")
+# else:
+#     print("It's too cold outside")
+# print("Done")
